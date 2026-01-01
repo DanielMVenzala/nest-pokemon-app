327d90fa8b5775d71e7c6cffa18068334ae5c8ad
DanielMVenzala <danielmvn@hotmail.com>
2026-01-01 20:23:06 +0100

    editando configuración docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { join } from 'path';
+import { join } from 'path'; // en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -15,20 +16,24 @@ import { JoiValidationSchema } from './config/joi.validation';
       load: [EnvConfiguration],
       validationSchema: JoiValidationSchema,
     }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
+    // -- select url, 'http://localhost:3000/api/files/product/' || url from product_images;
+
+    // -- update product_images set url = 'http://localhost:3000/api/files/product/' || url
+
     MongooseModule.forRoot(process.env.MONGODB!, {
       dbName: 'pokemonsdb',
     }),
+
     PokemonModule,
+
     CommonModule,
+
     SeedModule,
   ],
 })
-export class AppModule {
-  // constructor() {
-  //   console.log(process.env);
-  // }
-}
+export class AppModule {}
